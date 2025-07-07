3fdea7b45f4a359f6aa484a5b34801747d7d4f1e
Charlie Wilson <me@charliewilson.co.uk>
2025-07-07 12:46:01 +0100

    Feature: exception template can now be overridden with an exception.twig in app/templates

--- src/Exceptions/ExceptionRenderer.php
+++ src/Exceptions/ExceptionRenderer.php
@@ -14,9 +14,18 @@ class ExceptionRenderer
   private Environment $twig;
 
   public function __construct() {
-    $this->twig   = new Environment((new FilesystemLoader(__DIR__ . "/../Templates")),[
-        'debug' => true
-    ]);
+
+    // Override default exception template if exception.twig is present in conduit template root
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/../app/templates/exception.twig")) {
+      $this->twig   = new Environment((new FilesystemLoader($_SERVER['DOCUMENT_ROOT'] . "/../app/templates")),[
+          'debug' => true
+      ]);
+    } else {
+      $this->twig   = new Environment((new FilesystemLoader(__DIR__ . "/../Templates")),[
+          'debug' => true
+      ]);
+    }
+
     $this->twig->addExtension(new DebugExtension());
     $this->twig->addExtension(new IntlExtension());
   }
