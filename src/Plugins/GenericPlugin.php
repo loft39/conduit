0244dbe865c87316641bc7e0ea9dcdecf6bc604a
Charlie Wilson <me@charliewilson.co.uk>
2026-02-11 11:19:29 +0000

    Fix: `GenericPlugin::mountedPlugins` superseded by `GenericPlugin::pluginManager`

--- src/Plugins/GenericPlugin.php
+++ src/Plugins/GenericPlugin.php
@@ -11,7 +11,7 @@ class GenericPlugin
   private string $className;
   protected array $config;
   protected RouterController $router;
-  protected array $mountedPlugins;
+  protected PluginManager $pluginManager;
 
   public function __construct(PluginManager $pm) {
     $namespacedClass = explode("\\", get_class($this));
@@ -22,7 +22,7 @@ class GenericPlugin
     $this->config = $fullConfig['plugins'][$this->className] ?? Array();
 
     $this->router = $pm->routerController();
-    $this->mountedPlugins = $pm->mountedPlugins();
+    $this->pluginManager = $pm;
   }
 
 }
